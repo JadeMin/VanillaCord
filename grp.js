bf72f1a2a737d7b62f5836adefb404430c10b2a7
KlartNET <36400787+JadeMin@users.noreply.github.com>
2023-02-14 11:05:01 +0900

    Update grp.js

--- grp.js
+++ grp.js
@@ -1,2 +1,37 @@
-eval(await (await fetch('https://gist.githubusercontent.com/JadeMin/783dc38e56a67ed6ab0b4d8f75d27677/raw/ab6554f746b18fb69e9206a7fa3e39255a27b914/getModule.js')).text());
-getModule(m=> m?.getName?.() === 'UserStore')[0].getCurrentUser().premiumType = 2;
+let logging = false;
+function cLog(){
+	if(logging) console.log(...arguments);
+}
+if(!window.hasOwnProperty("WebpackExports")) {
+	window.WebpackExports = window.webpackChunkdiscord_app.push([["MODULE_MANAGER"], {}, e => e]);
+	cLog("WebpackChunk pushed");
+} else cLog("already WebpackChunk pushed");
+window.getModule(filter=(m => m)) {
+	const result = [];
+	const modules = WebpackExports.c;
+	const fixedFilter = (module) => {
+		try {
+			return filter(module);
+		} catch(error){
+			return false;
+		}
+	};
+
+	for(const index in modules) {
+		if(!modules.hasOwnProperty(index)) return;
+		const exports = modules[index].exports;
+
+		if(exports) {
+			if( (exports.default && fixedFilter(exports.default)) && exports.__esModule ) {
+				cLog("exports.default");
+				result.push(exports.default);
+			} else if( fixedFilter(exports) ) {
+				cLog("filter(modules)");
+				result.push(exports);
+			} else continue;
+		}
+	}
+	return result;
+};
+
+window.getModule(m=> m?.getName?.() === 'UserStore')[0].getCurrentUser().premiumType = 2;
